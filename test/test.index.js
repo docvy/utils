8ca6a48ffc7d4a26fdcaf9c4525e3698c65868f6
GochoMugo <mugo@forfuture.co.ke>
2015-07-28 12:55:54 +0300

    fix tests

--- test/test.index.js
+++ test/test.index.js
@@ -23,13 +23,12 @@ var utils = require("../lib/index");
 
 
 describe("utils module", function() {
-  it("should export the builtin `util` module, AS IS", function() {
-    var util = require("util");
-    for (var key in util) {
-      should(util[key]).eql(utils[key]);
-    }
-  });
-
+    it("should export the builtin `util` module, AS IS", function() {
+        var util = require("util");
+        for (var key in util) {
+            should(util[key]).eql(utils[key]);
+        }
+    });
 });
 
 
@@ -41,50 +40,51 @@ describe("utils.errors", function() {
 
 
 describe("utils.defineCallback", function() {
-  it("returns a function if no callback is passed", function() {
-    utils.defineCallback().should.be.a.Function();
-  });
-
-  it("callback passed must be a function", function() {
-    var _types = [[], [1], "", "string", -1, 0, 1, {}, { name: "mocha" }, true, false];
-    _types.forEach(function(_type) {
-      utils.defineCallback(_type).should.not.eql(_type).and.be.a.Function();
+    it("returns a function if no callback is passed", function() {
+        utils.defineCallback().should.be.a.Function();
     });
-  });
 
+    it("callback passed must be a function", function() {
+        var _types = [[], [1], "", "string", -1, 0, 1, {}, { name: "mocha" }, true, false];
+        _types.forEach(function(_type) {
+            utils.defineCallback(_type).should.not.eql(_type).and.be.a.Function();
+        });
+    });
 });
 
 
 describe("utils.getPath", function() {
-  it("returns application-specific paths", function() {
-    var homePath = utils.getPath("app.home");
-    var cachePath = utils.getPath("app.cache");
-    var pluginsCachePath = utils.getPath("app.cache.plugins");
-    var logsPath = utils.getPath("app.logs");
-    var pluginsPath = utils.getPath("app.plugins");
-    var paths = [homePath, cachePath, pluginsCachePath,
-      logsPath, pluginsPath];
-    paths.forEach(function(_path) {
-      _path.should.be.a.String();
+    it("returns application-specific paths", function() {
+        var homePath = utils.getPath("app.home");
+        var cachePath = utils.getPath("app.cache");
+        var pluginsCachePath = utils.getPath("app.cache.plugins");
+        var logsPath = utils.getPath("app.logs");
+        var pluginsPath = utils.getPath("app.plugins");
+        var paths = [
+            homePath, cachePath, pluginsCachePath,
+            logsPath, pluginsPath,
+        ];
+        paths.forEach(function(_path) {
+            _path.should.be.a.String();
+        });
     });
-  });
 
-  it("returns null if no path is found", function() {
-    should(utils.getPath()).be.null();
-    should(utils.getPath("non-existing-path")).be.null();
-  });
+    it("returns null if no path is found", function() {
+        should(utils.getPath()).be.null();
+        should(utils.getPath("non-existing-path")).be.null();
+    });
 
-  it("creates the path if it does not exist", function() {
-    var pathToLogs = utils.getPath("app.logs");
-    var tmpPathToLogs = pathToLogs + "_tmp";
-    try {
-      fs.renameSync(pathToLogs, tmpPathToLogs);
-    } catch (err) { /* may be we dont have logs yet */ }
-    pathToLogs = utils.getPath("app.logs");
-    should(fs.existsSync(pathToLogs)).eql(true);
-    fs.rmdirSync(pathToLogs);
-    try {
-      fs.renameSync(tmpPathToLogs, pathToLogs);
-    } catch (err) { /* may be we dont have logs yet */ }
-  });
+    it("creates the path if it does not exist", function() {
+        var pathToLogs = utils.getPath("app.logs");
+        var tmpPathToLogs = pathToLogs + "_tmp";
+        try {
+            fs.renameSync(pathToLogs, tmpPathToLogs);
+        } catch (err) { /* may be we dont have logs yet */ }
+        pathToLogs = utils.getPath("app.logs");
+        should(fs.existsSync(pathToLogs)).eql(true);
+        fs.rmdirSync(pathToLogs);
+        try {
+            fs.renameSync(tmpPathToLogs, pathToLogs);
+        } catch (err) { /* may be we dont have logs yet */ }
+    });
 });
